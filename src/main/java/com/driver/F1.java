75c712058d7d6015e819c3bd273169770ca223c2
AbhijeetKambare <abhijeetkambare1999@gmail.com>
2023-01-21 16:18:29 +0530

    first

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,13 +1,18 @@
 package com.driver;
 
 public class F1 extends Car {
+    //private boolean isManual;
+
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,2,6,isManual,"F1",1);
+      //  this.isManual=isManual;
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
+        int newGear=0;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,9 +25,25 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            newGear=1;
         }
         //for all other cases, change the gear accordingly
+        else if (newSpeed>0 && newSpeed<=50) {
+            newGear = 1;
+        }else if(newSpeed>50 && newSpeed<=100){
+            newGear=2;
+        } else if (newSpeed>100 && newSpeed<=150) {
+            newGear=3;
+        }else if(newSpeed>150 && newSpeed<=200){
+            newGear=4;
+        } else if (newSpeed>200 && newSpeed<=250) {
+            newGear=5;
+        } else if (newSpeed>250) {
+            newGear=6;
+        }
 
+        changeGear(newGear);
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,6 +5,9 @@ public class Main {
         F1 mercedes = new F1("mercedes", false);
         mercedes.move(40, 0);
         mercedes.accelerate(20);
+        //mercedes.accelerate(40);
         mercedes.accelerate(-60);
+
+       // Car t1=new Car("maruti",4,4,5,true,"opentop",4);
     }
 }
